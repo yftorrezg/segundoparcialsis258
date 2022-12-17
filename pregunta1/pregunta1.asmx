5ab86df5718de1332576ae87e412017ba61dc268
Fernando Torrez <Fer.torrez@outlook.es>
2022-12-17 14:04:07 -0400

    pregunta1

---
+++ pregunta1/pregunta1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="vb" CodeBehind="pregunta1.asmx.vb" Class="pregunta1.pregunta1" %>
---
+++ pregunta1/pregunta1.asmx.vb
@@ -0,0 +1,30 @@
+﻿Imports System.ComponentModel
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+
+' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
+' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<ToolboxItem(False)> _
+Public Class pregunta1
+    Inherits System.Web.Services.WebService
+
+    <WebMethod()> _
+    Public Function HelloWorld() As String
+        Return "Hola a todos"
+    End Function
+
+    <WebMethod()>
+    Public Function obtenerCotizacion(ByVal moneda As String) As Double
+        Dim cotizacion As Double
+        If moneda = "US" Then
+            cotizacion = 6.96
+        Else
+            cotizacion = 8.5
+        End If
+        Return cotizacion
+    End Function
+
+
+End Class
